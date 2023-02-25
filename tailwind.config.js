cbd7993f9c0e4d54f4aa0756fc9c566e6b3577ba
MunBrian <bryanmungai254@gmail.com>
2023-02-26 00:37:48 +0300

    Created UI

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      "white": "#FDFDFD",
+      "darkgrey": "#575D68",
+      "lightgreen": "#04C35C",
+      "lightblack": "#262626",
+      "black": "#030303",
+      "darkblue": "#174f69",
+      "lightblue": "#206e92",
+      "lightgrey": "#B1B4B9",
+      "darkgreen": "#04af53"
+    
+    }
+
+  },
+  plugins: [],
+}
